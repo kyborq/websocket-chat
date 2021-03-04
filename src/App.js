52cd031ed3e25497621b79662c677709830568dd
Konstantin <k.podyganov@mail.ru>
2021-03-05 00:41:09 +0300

    Added chat page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router , Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { HomePage } from "./pages/HomePage";
-import { AboutPage } from "./pages/AboutPage";
+import { ChatPage } from "./pages/ChatPage";
 
 import "./styles.css";
 
@@ -10,8 +9,7 @@ export default function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/" component={ChatPage} />
       </Switch>
     </Router>
   );
