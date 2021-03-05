37f755feacf73dc3caf3d3d4dede0a5b14342442
Konstantin <k.podyganov@mail.ru>
2021-03-05 11:51:55 +0300

    Added home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import { ChatPage } from "./pages/ChatPage";
+import { HomePage } from "./pages/HomePage";
 
 import "./styles.css";
 
@@ -9,7 +10,8 @@ export default function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={ChatPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/chat" component={ChatPage} />
       </Switch>
     </Router>
   );
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Page } from "../components/Page";
+
+export function HomePage() {
+  return (
+    <Page></Page>
+  );
+}
